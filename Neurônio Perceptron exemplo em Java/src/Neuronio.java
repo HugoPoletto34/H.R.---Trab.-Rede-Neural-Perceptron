1bdc6713f64c4eb62703f98f78392df35dd21b65
HugoPoletto34 <67022441+HugoPoletto34@users.noreply.github.com>
2020-12-03 08:41:33 -0300

    trocando matriz para vetor

--- Neurônio Perceptron exemplo em Java/src/Neuronio.java
+++ Neurônio Perceptron exemplo em Java/src/Neuronio.java
@@ -1,29 +1,30 @@
 public class Neuronio {
     public static void main(String[] args) {
-        int tam = 5;
-        int mat[][] = new int[tam][3];
+        int tam = 50;
+        int vet[] = new int[tam * 3];
 
         double w1 =  0.0;
         double w2 = 0.8;
 
-        double taxaApre = 0.05;
+        double taxaApre = 0.0001;
+        int cont = 0;
+        for (int i = 0; i < tam * 3; i+=3) {
 
-        for (int i = 0; i < tam; i++) {
-            int j = 0;
-            mat[i][j] = i + 1;
-            mat[i][j + 1] = i + 1;
-            mat[i][j + 2] = mat[i][j] + mat[i][j + 1];
+            vet[i] = cont + 1;
+            vet[i + 1] = cont + 1;
+            vet[i + 2] = vet[i] + vet[i + 1];
+            cont++;
         }
 
         double erro;
-        for (int i = 0; i < tam; i++) {
-            int j = 0;
-            erro = mat[i][j + 2] - (mat[i][j] * w1 + mat[i][j + 1] * w2);
-            w1 = w1 + erro * taxaApre * mat[i][j];
-            w2 = w2 + erro * taxaApre * mat[i][j + 1];
+        for (int i = 0; i < tam * 3; i+=3) {
+
+            erro = vet[i + 2] - (vet[i] * w1 + vet[i + 1] * w2);
+            w1 = w1 + erro * taxaApre * vet[i];
+            w2 = w2 + erro * taxaApre * vet[i + 1];
         }
 
-        for (int i = 0; i < tam + 10; i++) {
+        for (int i = 0; i < tam * 3 + 500; i++) {
             System.out.println((i + 1) + " + " + (i + 1) + " = " + ((i + 1) * w1 + (i + 1) * w2));
         }
     }
