45089ca6f0c49876fa9ed44fe3294d094fe2ed51
Rafaella Cristina <67807996+RafaellaCristina@users.noreply.github.com>
2020-12-15 23:10:14 -0300

    Update main.c

--- neurônio Perceptron em C/main.c
+++ neurônio Perceptron em C/main.c
@@ -1,39 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
+
+
 int main()
 {
-    setlocale(LC_ALL, "portuguese");
 
-    float dadoA = 0, dadoB = dadoA;
-    float valorA, valorB;
-    float pesoA = 1.3, pesoB = 2.2;
-    float multA, multB;
-    float taxaDeAprendizado = 0.0005;
-    float saidaDesejada;
-    float saidaNeuronio;
-    float erro;
-    int etapa = 5;
+    int tam = 50;
+    int vet[tam*3];
+    float w1 = 1.3;
+    float w2 = 2.2;
 
-    for (int i = 0; i < etapa; i++)
+    float taxaApre = 0.0005;
+    int etapa = 3;
+    int contador = 1;
+    while(contador < etapa)
     {
-        printf("\n %i ° Etapa:\n", etapa);
-        for(int j = 0; j < 5; j++)
-        {
+    printf("\n %i Etapa \n", contador);
+           int cont = 0;
+                for (int i = 0; i < tam * 3; i+=3)
+               {
 
-        dadoA = dadoA + 3;
-        dadoB = dadoB + 3;
-        saidaDesejada = dadoA + dadoB;
-        valorA = dadoA * pesoA;
-        valorB = dadoB * pesoB;
-        saidaNeuronio = valorA + valorB;
-        erro = saidaDesejada - saidaNeuronio;
-        printf("Sendo a soma de %f e %f o resultado foi: %f sendo a saida desejada: %f \n", dadoA, dadoB, saidaNeuronio, saidaDesejada);
-        multA = erro * taxaDeAprendizado * dadoA;
-        pesoA = pesoA + multA;
-        multB = erro * taxaDeAprendizado * dadoB;
-        pesoB = pesoB + multB;
-        }
+                    vet[i] = cont + 3;
+                    vet[i + 1] = cont + 3;
+                    vet[i + 2] = vet[i] + vet[i + 1];
+                    cont++;
+                }
+
+
+            for (int i = 0; i < tam * 3 + 6; i++)
+            {
+                int tempInt = i + 3;
+                float tempFloat = tempInt * w1 + tempInt * w2;
+                printf (" %i + %i = %.2f\n", tempInt, tempInt, tempFloat);
+
+            }
+
+            float erro;
+            for (int i = 0; i < tam * 3; i+=3)
+            {
+                erro = vet[i + 2] - (vet[i] * w1 + vet[i + 1] * w2);
+                w1 = w1 + erro * taxaApre * vet[i];
+                w2 = w2 + erro * taxaApre * vet[i + 1];
+            }
+    contador++;
     }
     system("pause");
     return 0;
